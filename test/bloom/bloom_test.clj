c77d73add41511c366901cd20aa21c52fabd2913
Peter Vlk <petervlk86@gmail.com>
2024-03-26 00:20:54 +0100

    initial commit

---
+++ test/bloom/bloom_test.clj
@@ -0,0 +1,7 @@
+(ns bloom.bloom-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [bloom.bloom :as sut]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 (sut/foo 1)))))
